053eaaeef6928f0296dd434d702ec719c136b8d8
root <root@mqtt.luvfa3tehlwuzdfrhtnjgucuse.ix.internal.cloudapp.net>
2020-03-14 11:04:44 +0000

    test

--- final/pinghardware.php
+++ final/pinghardware.php
@@ -1,12 +1,6 @@
 <?php
-function pingAddress($ip) {
-    $pingresult = exec("/bin/ping -n 3 $ip", $outcome, $status);
-    if (0 == $status) {
-        $status = "alive";
-    } else {
-        $status = "dead";
-    }
-    echo "The IP address, $ip, is  ".$status;
-}
 
-pingAddress("127.0.0.1");
+$currentdata = date("Y-m-d:H:i:s");
+echo $currentdata;
+
+?>
