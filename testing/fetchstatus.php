9745e2e0ea99af077987ef7a1a75e04bf6211543
root <root@mqtt.luvfa3tehlwuzdfrhtnjgucuse.ix.internal.cloudapp.net>
2020-03-21 14:03:59 +0000

    testing

---
+++ testing/fetchstatus.php
@@ -0,0 +1,16 @@
+<?php include('connect.php'); ?>
+<?php
+$device=$_GET['id'];
+   $qry = "select * from devicestatus where deviceid = '$device'";
+    $res = mysqli_query($link, $qry);
+
+        $emparr = array();
+    while($row =mysqli_fetch_assoc($res))
+    {
+        $emparr[] = $row;
+    }
+    echo json_encode($emparr);
+
+    mysqli_close($connection);
+
+?>
