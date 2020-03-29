a9ac90a98a4d22ceae4d7ff4d03991da3085c510
root <root@mqtt.uadqzj31xmnedcnyqz2bq5ca4e.ix.internal.cloudapp.net>
2020-03-29 18:04:33 +0000

    ansible-update

--- api/lastid.php
+++ api/lastid.php
@@ -1,17 +1,18 @@
 <?php include('connect.php'); ?>
 <?php
 #$device=$_GET['id'];
-   $qry = "SELECT * FROM `devicestatus` LIMIT 1";
-    $res = mysqli_query($link, $qry);
+   $qry = "SELECT portid FROM `devicestatus` LIMIT 1";
+$res = mysqli_query($link, $qry);
+$data = mysqli_fetch_array($res);
+	echo $data['portid'];
+#        $emparr = array();
+#    while($row =mysqli_fetch_assoc($res))
+#    {
+#        $emparr[] = $row;
+#    }
+#    echo json_encode($emparr);
 
-        $emparr = array();
-    while($row =mysqli_fetch_assoc($res))
-    {
-        $emparr[] = $row;
-    }
-    echo json_encode($emparr);
-
-    mysqli_close($connection);
+#    mysqli_close($connection);
 
 ?>
 
